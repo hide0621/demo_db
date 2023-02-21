c20a7dc933e73b31910cc0a1ae0bb5ced7e7f7c6
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-21 11:02:27 +0900

    getAllUsersとaddNewUserのテストを作成した

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testImplementation("junit:junit:4.13.2")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/com/example/hello_db_app/demo_db/controllers/ControllerTests.kt
@@ -0,0 +1,53 @@
+package com.example.hello_db_app.demo_db.controllers
+
+import com.example.hello_db_app.demo_db.MainController
+import com.example.hello_db_app.demo_db.Users
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest
+class ControllerTests {
+
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var target: MainController
+
+    @Before
+    fun setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(target).build()
+    }
+
+    @Test
+    fun getAllUsersTest() {
+        mockMvc.perform(
+            get("/all")
+        )
+            .andExpect(status().isOk)
+            .andExpect(
+                content().contentType(MediaType.APPLICATION_JSON_UTF8)
+            )
+    }
+
+    @Test
+    fun addNewUserTest() {
+        mockMvc.perform(
+            post("/add")
+                .param("name", "unit_test")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().string("Saved"))
+    }
+
+}
